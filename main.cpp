a63fdcab4c06e6c94a7c8af7b3c3a49400538f12
jessicp0249 <42855762+jessicp0249@users.noreply.github.com>
2018-11-13 13:17:44 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main()
 #if 0
     Trans_log log;  // Create log - contains empty  vector<Transaction>, min balance, average balance
     log.fill();     // Fill vector with Transaction objects
-    cout << log.print();
+    
 
     // find and print deposits
     cout << "Deposits:" << endl;
@@ -20,6 +20,7 @@ int main()
     {
         if(log.get_entries[i].get_amount() > 0)
                 log.get_entries[i].print();
+        cout << "Balance: " << log.balance(i) << endl;
     }
     
     // find and print withdrawals
@@ -28,8 +29,12 @@ int main()
     {
         if(log.get_entries[i].get_amount() < 0)
                 log.get_entries[i].print();
+        cout << "Balance: " << log.balance(i) << endl;
     }
-
+    
+    // Print interest earned
+    cout << "Interest earned (minimum): " << log.get_minbal() << endl;
+    cout << "Interest earned (average): " << log.get_avbal() << endl;
 
 #endif
 
